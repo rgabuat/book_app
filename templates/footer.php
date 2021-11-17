0937d2a14e545d9b72f3dce2aa9b51510afb1755
Jannel <56280151+lacbainnel@users.noreply.github.com>
2021-11-17 19:48:56 +0800

    test remove

--- templates/footer.php
+++ templates/footer.php
@@ -15,8 +15,7 @@
       <!-- Section: CTA -->
     </div>
     <!-- Grid container -->
-    test
-
+  
     <!-- Copyright -->
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
       © 2020 Copyright:
