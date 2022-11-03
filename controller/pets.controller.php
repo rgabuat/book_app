78586c2a8cfd1f282a508518ebb5e79d9e25e676
rgabuat <johncarlgabuat@gmail.com>
2022-11-03 17:05:07 -0400

    crud dogs and cats

---
+++ controller/pets.controller.php
@@ -0,0 +1,169 @@
+<?php 
+session_start();
+include('../dbconnect.php');
+include('../functions.php');
+
+
+
+if(isset($_POST['update_cat']))
+{
+    $uid = $_POST['cid'];
+    $petAge = $_POST['petAge'];
+    $petAgeDD = $_POST['petAgeDD'];
+    $petAgeNum = $_POST['petAgeNum'];
+    $petDesc = $_POST['petDesc'];
+    $petID = $_POST['petID'];
+    $petName = $_POST['petName'];
+    $petSex = $_POST['petSex'];
+    // $petType = $_POST['petType'];
+    $q1 = $_POST['q1'];
+    $q2 = $_POST['q2'];
+    $q3 = $_POST['q3'];
+    $q4 = $_POST['q4'];
+    $q5 = $_POST['q5'];
+    $q6 = $_POST['q6'];
+    $q7 = $_POST['q7'];
+    $q8 = $_POST['q8'];
+    $q9 = $_POST['q9'];
+    $shelter = $_POST['shelter'];
+
+    $params = [
+        'petAge' => $petAge,
+        'petAgeDD' => $petAgeDD,
+        'petAgeNum' => $petAgeNum,
+        'petDesc' => $petDesc,
+        'petName' => $petName,
+        'petSex' => $petSex,
+        'q1' => $q1,
+        'q2' => $q2,
+        'q3' => $q3,
+        'q4' => $q4,
+        'q5' => $q5,
+        'q6' => $q6,
+        'q7' => $q7,
+        'q8' => $q8,
+        'q9' => $q9,
+        'shelter' => $shelter,
+    ];
+
+    $ref_table = 'Pets/Cats/'.$uid;
+    
+    $pet_updated = $database->getReference($ref_table)->update($params);
+
+    if($pet_updated)
+    {
+        header("location: ../dashboard.php?page=display-cats&id=".$uid."&action=update&status=success");
+        exit();
+    }
+    else 
+    {
+        header("location: ../dashboard.php?page=display-cats&id=".$uid."&action=update&status=error");
+        exit();
+    }
+
+}
+
+if(isset($_POST['delete_cat']))
+{
+    $uid = $_POST['cid'];
+
+    $ref_table = 'Pets/Cats/'.$uid;
+    
+    $pet_deleted = $database->getReference($ref_table)->remove();
+
+    if($pet_deleted)
+    {
+        header("location: ../dashboard.php?page=display-cats&id=".$uid."&action=delete&status=success");
+        exit();
+    }
+    else 
+    {
+        header("location: ../dashboard.php?page=display-cats&id=".$uid."&action=delete&status=error");
+        exit();
+    }
+
+}
+
+
+//dogs 
+
+if(isset($_POST['update_dog']))
+{
+    $uid = $_POST['cid'];
+    $petAge = $_POST['petAge'];
+    $petAgeDD = $_POST['petAgeDD'];
+    $petAgeNum = $_POST['petAgeNum'];
+    $petDesc = $_POST['petDesc'];
+    $petID = $_POST['petID'];
+    $petName = $_POST['petName'];
+    $petSex = $_POST['petSex'];
+    // $petType = $_POST['petType'];
+    $q1 = $_POST['q1'];
+    $q2 = $_POST['q2'];
+    $q3 = $_POST['q3'];
+    $q4 = $_POST['q4'];
+    $q5 = $_POST['q5'];
+    $q6 = $_POST['q6'];
+    $q7 = $_POST['q7'];
+    $q8 = $_POST['q8'];
+    $q9 = $_POST['q9'];
+    $shelter = $_POST['shelter'];
+
+    $params = [
+        'petAge' => $petAge,
+        'petAgeDD' => $petAgeDD,
+        'petAgeNum' => $petAgeNum,
+        'petDesc' => $petDesc,
+        'petName' => $petName,
+        'petSex' => $petSex,
+        'q1' => $q1,
+        'q2' => $q2,
+        'q3' => $q3,
+        'q4' => $q4,
+        'q5' => $q5,
+        'q6' => $q6,
+        'q7' => $q7,
+        'q8' => $q8,
+        'q9' => $q9,
+        'shelter' => $shelter,
+    ];
+
+    $ref_table = 'Pets/Dogs/'.$uid;
+    
+    $pet_updated = $database->getReference($ref_table)->update($params);
+
+    if($pet_updated)
+    {
+        header("location: ../dashboard.php?page=display-dogs&id=".$uid."&action=update&status=success");
+        exit();
+    }
+    else 
+    {
+        header("location: ../dashboard.php?page=display-dogs&id=".$uid."&action=update&status=error");
+        exit();
+    }
+
+}
+
+if(isset($_POST['delete_dog']))
+{
+    $uid = $_POST['cid'];
+
+    $ref_table = 'Pets/Dogs/'.$uid;
+    
+    $pet_deleted = $database->getReference($ref_table)->remove();
+
+    if($pet_deleted)
+    {
+        header("location: ../dashboard.php?page=display-dogs&id=".$uid."&action=delete&status=success");
+        exit();
+    }
+    else 
+    {
+        header("location: ../dashboard.php?page=display-dogs&id=".$uid."&action=delete&status=error");
+        exit();
+    }
+
+}
+
+?>
--- pages/display-dogs.php
+++ pages/display-dogs.php
@@ -21,26 +21,162 @@
 
                 if ($fetchdata > 0) {
                 $i = 1;
-                foreach($fetchdata as $key => $row) {
+                foreach($fetchdata as $key => $cats) {
             ?>
                 <td><?= $i++ ?></td>
-                <td><?= $row['petName'] ?></td>
-                <td><?= $row['petAge'] ?></td>
-                <td><?= $row['petSex'] ?></td>
-                <td><?= $row['petStatus'] ?></td>
+                <td><?= $cats['petName'] ?></td>
+                <td><?= $cats['petAge'] ?></td>
+                <td><?= $cats['petSex'] ?></td>
+                <td><?= $cats['petStatus'] ?></td>
                 <td>
-               <span>
-               <a id="editBtn" href="?page=accountedit&id=<?= $key; ?>" class="text-warning" data-id="<?= $key; ?>" data-title="Edit" data-action="edit" data-mdb-toggle="tooltip" data-mdb-placement="top" title="Edit">
-                    <i class="material-icons me-3">edit_note</i>
-                </a>
+                <span>
+                <a id="ActEditBtn" href="javascript:void(0);" class="text-warning" data-id="<?= $key?>" data-mdb-toggle="modal" data-mdb-target="#edit<?= $key?>" >
+                            <i class="material-icons me-3">edit_note</i>
+                        </a>
                </span>
                <span>
-               <a id="ActDelBtn" href="javascript:void(0);" class="text-danger" data-id="<?= $key?>" data-title="Delete" data-action="delete" data-mdb-toggle="tooltip" data-mdb-placement="top" title="Delete" >
+               <a id="ActDelBtn" href="javascript:void(0);" class="text-danger"  data-id="<?= $key?>" data-mdb-toggle="modal" data-mdb-target="#delete<?= $key?>" >
                     <i class="material-icons me-3">delete</i>
                 </a>
                </span>  
                 </td>
+                <!-- edit cat modal -->
+                <div
+                        class="modal fade"
+                        id="edit<?= $key?>"
+                        tabindex="-1"
+                        aria-labelledby="AccountEditModal"
+                        aria-hidden="true"
+                        >
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel"><span id="title" class="text-uppercase"></span>Edit Cat <strong> <?= $cats['petName'] ?></strong></h5>
+                                    <button
+                                    type="button"
+                                    class="btn-close"
+                                    data-mdb-dismiss="modal"
+                                    aria-label="Close"
+                                    ></button>
+                                </div>
+                            <div class="modal-body">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <form action="./controller/pets.controller.php" method="POST" class="" enctype='multipart/form-data'>
+                                            <input type="hidden" name="action" value="">
+                                            <input type="hidden" name="cid" value="<?= $key ?>">
+                                            <input type="hidden" name="petID" value="<?= $cats['petID'] ?>" class="form-control form-control rounded-0" required />
+                                            <div id="alert"></div>
+                                            <div  class="form-outline mb-3">
+                                                <input type="text" name="imageName" value="<?= isset($cats['imageName'])  ?  $cats['imageName'] : 'no image name' ;?>" class="form-control rounded-0" required />
+                                                <label class="form-label" for="Image">Image name</label>
+                                            </div>
+                                            <div  class="form-outline mb-3">
+                                                <input type="text" name="petName" value="<?= $cats['petName'] ?>" class="form-control rounded-0" required />
+                                                <label class="form-label" for="Name">Name</label>
+                                            </div>
+                                             <div class="form-outline mb-3">
+                                                <input type="text" name="petAge" value="<?= $cats['petAge'] ?>" class="form-control  rounded-0" required />
+                                                <label class="form-label" for="Age">Age</label>
+                                            </div>
+                                            <div class="form-outline mb-3">
+                                                <input type="text" name="petAgeDD" value="<?= isset($cats['petAgeDD'])  ?  $cats['petAgeDD'] : 'no data' ;?>" class="form-control rounded-0" required />
+                                                <label class="form-label" for="Age DD">Age DD</label>
+                                            </div>
+                                            <div class="form-outline mb-3">
+                                                <input type="text" name="petAgeNum" value="<?= isset($cats['petAgeNum'])  ?  $cats['petAgeNum'] : 'no data' ;?>" class="form-control rounded-0" required />
+                                                <label class="form-label" for="Age Num">Age Num</label>
+                                            </div>
+                                            <small><label class="form-label" for="Pet Sex">Pet Sex</label></small>
+                                            <select name="petSex" class="form-control mb-3">
+                                                <option value="">Select</option>
+                                                <option <?= $cats['petSex'] == 'Male' ? 'selected' : '' ?> value="<?= $cats['petSex'] ?>">Male</option>
+                                                <option <?= $cats['petSex'] == 'Female' ? 'selected' : '' ?> value="<?= $cats['petSex'] ?>">Female</option>
+                                            </select>
+                                            <p><strong>Pet Status:</strong><span class="badge badge-<?= $cats['petStatus'] == 'Available' ? 'success' : 'danger' ?>"><?= $cats['petStatus'] ?></span></p>
+                                            <p class="mb-3">Questions:</p>
+                                            <div class="form-outline mb-3">
+                                                <input type="text" name="q1" value="<?= $cats['q1'] ?>" class="form-control form-control rounded-0" required />
+                                                <label class="form-label" for="Pet ID">Question 1</label>
+                                            </div>
+                                            <div class="form-outline mb-3">
+                                                <input type="text" name="q2" value="<?= $cats['q2'] ?>" class="form-control form-control rounded-0" required />
+                                                <label class="form-label" for="Pet ID">Question 2</label>
+                                            </div>
+                                            <div class="form-outline mb-3">
+                                                <input type="text" name="q3" value="<?= $cats['q3'] ?>" class="form-control form-control rounded-0" required />
+                                                <label class="form-label" for="Pet ID">Question 3</label>
+                                            </div>
+                                            <div class="form-outline mb-3">
+                                                <input type="text" name="q4" value="<?= $cats['q4'] ?>" class="form-control form-control rounded-0" required />
+                                                <label class="form-label" for="Pet ID">Question 4</label>
+                                            </div>
+                                            <div class="form-outline mb-3">
+                                                <input type="text" name="q5" value="<?= $cats['q5'] ?>" class="form-control form-control rounded-0" required />
+                                                <label class="form-label" for="Pet ID">Question 5</label>
+                                            </div>
+                                            <div class="form-outline mb-3">
+                                                <input type="text" name="q6" value="<?= $cats['q6'] ?>" class="form-control form-control rounded-0" required />
+                                                <label class="form-label" for="Pet ID">Question 6</label>
+                                            </div>
+                                            <div class="form-outline mb-3">
+                                                <input type="text" name="q7" value="<?= $cats['q7'] ?>" class="form-control form-control rounded-0" required />
+                                                <label class="form-label" for="Pet ID">Question 7</label>
+                                            </div>
+                                            <div class="form-outline mb-3">
+                                                <input type="text" name="q8" value="<?= $cats['q8'] ?>" class="form-control form-control rounded-0" required />
+                                                <label class="form-label" for="Pet ID">Question 8</label>
+                                            </div>
+                                            <div class="form-outline mb-3">
+                                                <input type="text" name="q9" value="<?= $cats['q9'] ?>" class="form-control form-control rounded-0" required />
+                                                <label class="form-label" for="Pet ID">Question 9</label>
+                                            </div>
+                                            <div class="form-outline mb-3">
+                                                <input type="text" name="shelter" value="<?= $cats['shelter'] ?>" class="form-control form-control rounded-0" required />
+                                                <label class="form-label" for="Pet ID">Shelter</label>
+                                            </div>
+                                            <div class="form-outline mb-3">
+                                                <textarea name="petDesc" cols="30" rows="10" class="form-control form-control rounded-0" required><?= $cats['petDesc'] ?></textarea>
+                                                <label class="form-label" for="dssc">Age</label>
+                                            </div>
+                                           
+                                            <input type="submit" name="update_dog" class="btn btn-success form-control my-3 rounded-0" data-mdb-ripple-color="dark" value="SUBMIT">
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </tr>
+
+                <!-- delete cat modal -->
+                <div
+                        class="modal fade"
+                        id="delete<?= $key?>"
+                        aria-labelledby="catDeleteModal"
+                        >
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="dogDeleteModal"><span id="title-2" class="text-uppercase"></span>Delete Cat</h5>
+                                    <button
+                                    type="button"
+                                    class="btn-close"
+                                    data-mdb-dismiss="modal"
+                                    aria-label="Close"
+                                    ></button>
+                                </div>
+                            <div class="modal-body">
+                                <div class="row">
+                                    <form action="./controller/pets.controller.php" method="POST" class="" enctype='multipart/form-data'>
+                                        <h3><p>Delete Cat </p></h3>
+                                        <input  type="hidden" name="cid" value="<?= $key ?>">
+                                        <input type="submit" name="delete_dog" class="btn btn-success form-control my-3 rounded-0" data-mdb-ripple-color="dark" value="SUBMIT">
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 <?php
                 }
                 } else {
@@ -53,44 +189,4 @@
             ?>
         </tbody>
     </table>
-
-    <!-- modal -->
-
-    <div
-        class="modal fade"
-        id="AccountEditModal"
-        tabindex="-1"
-        aria-labelledby="AccountEditModal"
-        aria-hidden="true"
-        >
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel"><span id="title" class="text-uppercase"></span> ACCOUNT</h5>
-                    <button
-                    type="button"
-                    class="btn-close"
-                    data-mdb-dismiss="modal"
-                    aria-label="Close"
-                    ></button>
-                </div>
-            <div class="modal-body">
-            <div class="row">
-                <form action="./controller/users.controller.php" method="POST" class="" enctype='multipart/form-data'>
-                    <input id="action" type="hidden" name="action" value="">
-                    <input id="editId" type="hidden" name="editId" value="">
-                    <div id="alert"></div>
-                    <div id="editContainer" class="form-outline">
-                        <input type="text" id="authorName" name="author" value="" class="form-control form-control-lg rounded-0" required />
-                        <label class="form-label" for="formControlLg">Account</label>
-                    </div>
-                    <input type="submit" name="submit" class="btn btn-success form-control my-3 rounded-0" data-mdb-ripple-color="dark" value="SUBMIT">
-                </form>
-            </div>
-            </div>
-        </div>
-    </div>
-
-
-
 </div>
